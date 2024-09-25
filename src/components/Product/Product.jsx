7fcd5c85fbba52f74eb96daee044e2e52394a554
birukjira <birukjira@gmail.com>
2024-09-25 17:00:00 +0300

    product fix

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
-import classes from "./product.module.css";
+import classes from "./Product.module.css";
 import Loader from "../Loader/Loader";
 
 function Product() {
